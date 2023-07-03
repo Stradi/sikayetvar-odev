496dbabca8683ead82cb4d60685b65b534b2ec8f
Batın Evirgen <batinevirgen@gmail.com>
2023-07-03 14:32:36 +0300

    feat: update homepage to show implemented pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,23 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <main>Hello, world!</main>;
+  return (
+    <main className="flex h-screen w-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-semibold">Implemented Pages</h1>
+      <ul className="flex gap-2 text-center">
+        <Link passHref className="hover:underline" href="/login">
+          <li>Login</li>
+        </Link>
+        <Link passHref className="hover:underline" href="/dashboard">
+          <li>Dashboard Overview</li>
+        </Link>
+        <Link passHref className="hover:underline" href="/dashboard/students">
+          <li>Students</li>
+        </Link>
+        <Link passHref className="hover:underline" href="/404">
+          <li>404</li>
+        </Link>
+      </ul>
+    </main>
+  );
 }
